c255a4ccf9d079b2a9e2f3d2a27c6b4d24d01d39
Ricardo Ferreira <ricardo.ti.ads@gmail.com>
2020-04-28 23:13:19 -0300

    adicionando o caompo de somar e diminuir

--- public/js/validacoes.js
+++ public/js/validacoes.js
@@ -1,19 +1,50 @@
 $(document).ready(function () {
-    
-        // recebe o textarea
-        var campo = $('#descricao')
-        // passa as informaçoes digitadas
-        var digitado = campo.val().trim()
-        //devolve as informações sem espaço
-        $('#descricao').val(digitado)
-    
 
+    // recebe o textarea
+    var campo = $('#descricao')
+    // passa as informaçoes digitadas
+    var digitado = campo.val().trim()
+    //devolve as informações sem espaço
+    $('#descricao').val(digitado)
 })
+// diminue a soma 
+function diminuir() {
+    // inicia  a variavel com 1
+    var numero2 = 1;
+    var resultado;
+    // recebe o valor digitado na tela 
+    var numero = parseInt(document.getElementById("quantidade").value)
+    // verificao o que esta recebendo
+    if (numero === 0 || numero <= 1) {
+        //coloca o valor como 1
+        resultado = 1;
+        //retorna o valor
+        return document.getElementById("quantidade").value = resultado;
+    } else {
+        // faz a subtração
+        resultado = numero - numero2;
+        //retorna o valor
+        return document.getElementById("quantidade").value = resultado;
+    }
+
+}
+
+// aumanta a quantidade
+function aumentar() {
+    // inicia  a variavel com 1
+    var numero2 = 1;
+    // recebe o valor digitado na tela 
+    var numero = parseInt(document.getElementById("quantidade").value)
+    // faz a soma
+    var resultado = numero + numero2;
+    //retorna o valor
+    return document.getElementById("quantidade").value = resultado;
+}
 
 // função responsavel por fazer a deleção
-    function deletar(url, nome) {
-        if (window.confirm('Deseja realmente remover este ' + nome + '?')) {
-            window.location = url
-        }
+function deletar(url, nome) {
+    if (window.confirm('Deseja realmente remover este ' + nome + '?')) {
+        window.location = url
     }
+}
 
--- resources/views/venda/informacao_produto.blade.php
+++ resources/views/venda/informacao_produto.blade.php
@@ -6,9 +6,9 @@
         <!-- Image -->
         <div class="col-12 col-lg-6">
             <div class="card bg-light mb-3">
-                <div class="card-body">
+                <div class="card-body text-center">
                     <a href="" data-toggle="modal" data-target="#productModal">
-                        <img class="img-fluid" src="https://dummyimage.com/800x800/55595c/fff" />
+                        <img class="img-fluid" src="{{ url("storage/{$produto->imagem}") }}" />
                         <p class="text-center">Zoom</p>
                     </a>
                 </div>
@@ -19,36 +19,29 @@
         <div class="col-12 col-lg-6 add_to_cart_block">
             <div class="card bg-light mb-3">
                 <div class="card-body">
-                    <p class="price">99.00 $</p>
+         
+                    
                     <p class="price_discounted">149.90 $</p>
                     <form method="get" action="cart.html">
+                       <p class="price" id="valor" value="{{$produto->preco}}">R$ {{$produto->preco}}</p>
                         <div class="form-group">
-                            <label for="colors">Color</label>
-                            <select class="custom-select" id="colors">
-                                <option selected>Select</option>
-                                <option value="1">Blue</option>
-                                <option value="2">Red</option>
-                                <option value="3">Green</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label>Quantity :</label>
+                            <label>Quantidade :</label>
                             <div class="input-group mb-3">
                                 <div class="input-group-prepend">
-                                    <button type="button" class="quantity-left-minus btn btn-danger btn-number"  data-type="minus" data-field="">
+                                    <button type="button" onclick="diminuir()" class="quantity-left-minus btn btn-danger btn-number"  data-type="minus" data-field="">
                                         <i class="fa fa-minus"></i>
                                     </button>
                                 </div>
-                                <input type="text" class="form-control"  id="quantity" name="quantity" min="1" max="100" value="1">
+                                <input type="text" class="form-control" id="quantidade" name="quantidade" min="1" max="100" value="1">
                                 <div class="input-group-append">
-                                    <button type="button" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
+                                    <button type="button" onclick="aumentar()" class="quantity-right-plus btn btn-success btn-number" data-type="plus" data-field="">
                                         <i class="fa fa-plus"></i>
                                     </button>
                                 </div>
                             </div>
                         </div>
                         <a href="cart.html" class="btn btn-success btn-lg btn-block text-uppercase">
-                            <i class="fa fa-shopping-cart"></i> Add To Cart
+                            <i class="fa fa-shopping-cart"></i> Adicionar ao Carrinho
                         </a>
                     </form>
                     <div class="product_rassurance">
@@ -68,9 +61,7 @@
                         (4/5)
                         <a class="pull-right" href="#reviews">View all reviews</a>
                     </div>
-                    <div class="datasheet p-3 mb-2 bg-info text-white">
-                        <a href="" class="text-white"><i class="fa fa-file-text"></i> Download DataSheet</a>
-                    </div>
+                   
                 </div>
             </div>
         </div>
@@ -80,14 +71,12 @@
         <!-- Description -->
         <div class="col-12">
             <div class="card border-light mb-3">
-                <div class="card-header bg-primary text-white text-uppercase"><i class="fa fa-align-justify"></i> Description</div>
+                <div class="card-header bg-primary text-white text-uppercase"><i class="fa fa-align-justify"></i> Descrição</div>
                 <div class="card-body">
                     <p class="card-text">
-                        Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker.
+                        {{$produto->descricao }}
                     </p>
-                    <p class="card-text">
-                        Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du "De Finibus Bonorum et Malorum" (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsum, "Lorem ipsum dolor sit amet...", proviennent de la section 1.10.32.
-                    </p>
+                    
                 </div>
             </div>
         </div>
